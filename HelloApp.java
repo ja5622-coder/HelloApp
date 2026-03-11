46fa94dc87e15ebc47f26b6aae76b27d49f4795f
ja5622-coder <ja5622@srmist.edu.in>
2026-03-11 11:00:08 +0530

    Implemented UC2: Display Hello with command line argument

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,17 @@
-public class HelloApp {
+/**
+ * DisplayName.java - UC2: Display Hello with Command-Line Argument
+ * Accepts a user's name as a command-line argument and displays
+ * a personalized greeting.
+ */
 
+class DisplayName {
     public static void main(String[] args) {
 
-        if(args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello");
-        }
+        // Get the name from command-line argument
+        String name = args[0];
+
+        // Display personalized greeting
+        System.out.println("Hello, " + name + "!");
 
     }
-
 }
