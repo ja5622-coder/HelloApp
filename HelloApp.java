c9d11582db677e1785f4feb1d7853df77a755ca6
ja5622-coder <ja5622@srmist.edu.in>
2026-03-18 08:30:50 +0530

    Coded UC3: Display Hello using ternary operator

--- HelloApp.java
+++ HelloApp.java
@@ -1,17 +1,6 @@
-/**
- * DisplayName.java - UC2: Display Hello with Command-Line Argument
- * Accepts a user's name as a command-line argument and displays
- * a personalized greeting.
- */
-
-class DisplayName {
+public class HelloApp {
     public static void main(String[] args) {
-
-        // Get the name from command-line argument
-        String name = args[0];
-
-        // Display personalized greeting
+        String name = (args.length > 0) ? args[0] : "World";
         System.out.println("Hello, " + name + "!");
-
     }
 }
